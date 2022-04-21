fce358d8e23e26c1514dc5d272f21b5843b6f919
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-04-21 23:20:45 +0600

    S04: make the status persistent

--- lua/hint_message.lua
+++ lua/hint_message.lua
@@ -1,21 +1,15 @@
-if wesnoth.interface.add_overlay_text == nil then
-    return {}
-end
-
-hint_label = {valid = false}
--- Takes 'caption' and 'message' arguments, similar to [message]
+local hint_message = {valid = false}
+local hint_text = ""
 function wesnoth.wml_actions.hint_message(cfg)
-    if hint_label.valid then
-        hint_label:remove()
+    if hint_message.valid then
+        hint_message:remove()
     end
-    if cfg.message and cfg.message ~= "" then
-        local text
+    if cfg.message and not cfg.remove then
+        hint_text = cfg.message
         if cfg.caption and cfg.caption ~= "" then
-            text = "<b>" .. cfg.caption .. "</b>\n" .. cfg.message
-        else
-            text = cfg.message
+            hint_text = "<b><big>" .. cfg.caption .. "</big></b>\n" .. hint_text
         end
-        hint_label = wesnoth.interface.add_overlay_text(text, {
+        hint_message = wesnoth.interface.add_overlay_text(hint_text, {
             size = 20,
             -- lavender RGB (230,230,250)
             color = "E6E6FA",
@@ -26,4 +20,10 @@ function wesnoth.wml_actions.hint_message(cfg)
             halign = "left"
         })
     end
+end
+
+wesnoth.persistent_tags.hint_message.read = wesnoth.wml_actions.hint_message
+
+function wesnoth.persistent_tags.hint_message.write(add)
+    add{message = hint_text}
 end
