7ebb1ce6d26345f40b590b2f74dfd2944ed537e9
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-04-22 01:11:59 +0600

    Create enchant_choice.lua

---
+++ lua/enchant_choice.lua
@@ -0,0 +1,9 @@
+function wesnoth.wml_actions.enchant_choice(cfg)
+   cfg = wml.literal(cfg)
+   for i, tag in ipairs(cfg) do
+        if tag[1] == "option" then
+            tag[2].label = tag[2].label:vformat{cost = tag[2].cost}
+        end
+    end
+    wesnoth.wml_actions.message(wml.tovconfig(cfg))
+end
