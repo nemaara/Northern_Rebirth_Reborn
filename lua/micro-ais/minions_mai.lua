2a2c5c3ee2649be6e5107a780b278956af25122e
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-05-03 01:48:56 +0600

    S08: more updates

--- lua/micro-ais/minions_mai.lua
+++ lua/micro-ais/minions_mai.lua
@@ -1,5 +1,11 @@
 local AH = wesnoth.require "ai/lua/ai_helper.lua"
 
+-- please note this Micro AI was specifically tailored 
+-- for this campaign's scenarios
+-- Might be possible to make it work for in general but 
+-- might not get time to get that done
+-- grad-school starts soon
+
 function wesnoth.micro_ais.minions_ai(cfg)
 	if (cfg.action ~= 'delete') then
 		if (not cfg.id) and (not wml.get_child(cfg, "filter")) then
@@ -7,16 +13,12 @@ function wesnoth.micro_ais.minions_ai(cfg)
 		end
 	end
 
-    -- wesnoth.message("the minions mai is executed for side " .. wesnoth.current.side)
-
 	local required_keys = {}
 	local optional_keys = { "id", "enemy_death_chance", "[filter]", "[filter_second]", "invert_order", "messenger_death_chance", "waypoint_loc", "waypoint_x", "waypoint_y" }
 	local score = cfg.ca_score or 300000
 	local CA_parms = {
 		ai_id = 'northlands_minions_ai',
 		{ ca_id = 'attack', location = 'ca_messenger_attack.lua', score = score },
-        --temporarily commented out until I can find a way to set the location properly
-        --{ ca_id = 'move_to_any_enemy', location="ca_move_to_any_enemy.lua", score = score -1 },
 		{ ca_id = 'escort_move', location = 'ca_messenger_escort_move.lua', score = score - 2 }
 	}
     return required_keys, optional_keys, CA_parms
