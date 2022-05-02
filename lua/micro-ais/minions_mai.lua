d631a8d45c0e1c5b7b3ec8a15cc02ff9dd0f6744
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-05-02 23:31:41 +0600

    add boss minions code

---
+++ lua/micro-ais/minions_mai.lua
@@ -0,0 +1,23 @@
+local AH = wesnoth.require "ai/lua/ai_helper.lua"
+
+function wesnoth.micro_ais.minions_ai(cfg)
+	if (cfg.action ~= 'delete') then
+		if (not cfg.id) and (not wml.get_child(cfg, "filter")) then
+			wml.error("Minions AI [micro_ai] tag requires either id= key or [filter] tag")
+		end
+	end
+
+    -- wesnoth.message("the minions mai is executed for side " .. wesnoth.current.side)
+
+	local required_keys = {}
+	local optional_keys = { "id", "enemy_death_chance", "[filter]", "[filter_second]", "invert_order", "messenger_death_chance", "waypoint_loc", "waypoint_x", "waypoint_y" }
+	local score = cfg.ca_score or 300000
+	local CA_parms = {
+		ai_id = 'northlands_minions_ai',
+		{ ca_id = 'attack', location = 'ca_messenger_attack.lua', score = score },
+        --temporarily commented out until I can find a way to set the location properly
+        --{ ca_id = 'move_to_any_enemy', location="ca_move_to_any_enemy.lua", score = score -1 },
+		{ ca_id = 'escort_move', location = 'ca_messenger_escort_move.lua', score = score - 2 }
+	}
+    return required_keys, optional_keys, CA_parms
+end
